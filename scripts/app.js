b99f4ec3329fec866394bf59742b32729dddb5f4
Troy Miles <rockncoder@gmail.com>
2012-08-12 13:03:10 -0700

    Initial commit.

---
+++ scripts/app.js
@@ -0,0 +1,52 @@
+﻿
+
+
+var RocknCoder = RocknCoder || {};
+RocknCoder.Pages = RocknCoder.Pages || {};
+
+RocknCoder.Pages.Kernel = function (event) {
+	var that = this,
+		eventType = event.type,
+		pageName = $(this).attr("data-rockncoder-jspage");
+	if (RocknCoder && RocknCoder.Pages && pageName && RocknCoder.Pages[pageName] && RocknCoder.Pages[pageName][eventType]) {
+		RocknCoder.Pages[pageName][eventType].call(that);
+	}
+};
+
+RocknCoder.Pages.Events = (function () {
+	$("div[data-rockncoder-jspage]").on(
+		'pagebeforecreate pagecreate pagebeforeload pagebeforeshow pageshow pagebeforechange pagechange pagebeforehide pagehide pageinit',
+		RocknCoder.Pages.Kernel
+	);
+}());
+
+RocknCoder.Pages.page1 = (function () {
+	var pageinit = function () {
+		},
+		pageshow = function () {
+			window.scrollTo(1,0);
+		},
+		pagehide = function () {
+		};
+	return {
+		pageinit: pageinit,
+		pageshow: pageshow,
+		pagehide: pagehide
+	};
+}());
+
+RocknCoder.Pages.page2 = (function () {
+	var pageinit = function () {
+		},
+		pageshow = function () {
+			window.scrollTo(1,0);
+		},
+		pagehide = function () {
+		};
+	return {
+		pageinit: pageinit,
+		pageshow: pageshow,
+		pagehide: pagehide
+	};
+}());
+
